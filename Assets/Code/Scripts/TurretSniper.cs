5b9e8594f3c1e712b464da3a2f1ca8509b1ebe21
moh-dar709 <102563555+moh-dar709@users.noreply.github.com>
2024-04-02 16:22:33 -0400

    fixed merge with boosters

--- Assets/Code/Scripts/TurretSniper.cs
+++ Assets/Code/Scripts/TurretSniper.cs
@@ -151,5 +151,25 @@ public class TurretSniper : MonoBehaviour
         Handles.color = Color.cyan;
         Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
     }
+
+    public void SetBPS(float bullet_speed)
+    {
+        bps = bullet_speed;
+    }
+
+    public float GetBPS()
+    {
+        return bps;
+    }
+
+    public void SetDamage(int dmg)
+    {
+        damage = dmg;
+    }
+
+    public int GetDamage()
+    {
+        return damage;
+    }
 }
   
