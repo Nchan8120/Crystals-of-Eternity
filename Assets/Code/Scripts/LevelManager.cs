6e99ba83bf44bc9f0decc84bf39dd3ca35389a7d
moh-dar709 <102563555+moh-dar709@users.noreply.github.com>
2024-04-02 14:20:35 -0400

    Revert "Merge pull request #7 from Nchan8120/main"
    This reverts commit 4d8f5499e2957756afcea5e4037fec6d68429269, reversing
    changes made to 11963d0a07dfb406560e13c34322ae8b2cec6ac2.

--- Assets/Code/Scripts/LevelManager.cs
+++ Assets/Code/Scripts/LevelManager.cs
@@ -11,12 +11,9 @@ public class LevelManager : MonoBehaviour
     public Transform[] path;
     public Transform startPoint;
 
-    public int currency = 1000;
-    public int score = 0;
-    public int tower; // tower count
-    public int health = 100;
-
-    public int gamemode = 1; // 1 for time, 0 for health/waves
+    public int currency;
+    public int score;
+    public int tower;
 
     private void Awake()
     {
@@ -25,9 +22,7 @@ public class LevelManager : MonoBehaviour
 
     private void Start()
     {
-        currency = 1000;
-        score = 0;
-        health = 100;
+        currency = 100;
     }
 
     public void IncreaseCurrency(int amount)
@@ -39,6 +34,17 @@ public class LevelManager : MonoBehaviour
     public void IncreaseScore(int amount)
     {
         score += amount;
+        /* HOLDS HIGHSCORE USING PLAYER PREFS
+        if (PlayerPrefs.GetInt("HighScore") == 0)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+        if (PlayerPrefs.GetInt("HighScore") < score)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            print("HighScore set");
+        }
+        */
         Events.events["score"]?.Invoke(score);
     }
 
@@ -55,25 +61,5 @@ public class LevelManager : MonoBehaviour
             Debug.Log("not enough money");
             return false;
         }
-    }
-
-    public void DecreaseHealth(int amount)
-    {
-        this.health -= amount;
-    }
-
-    public void IncreaseHealth(int amount)
-    {
-        this.health += amount;
-    }
-
-    public string getPlayerScore()
-    {
-        return this.score.ToString();
-    }
-
-    public bool IsAlive()
-    {
-        return this.health > 0;
     }
 }
--- Assets/Menus/MMLogic.cs
+++ Assets/Menus/MMLogic.cs
@@ -5,25 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MMLogic : MonoBehaviour
 {
-
-    public GameObject playButton;
-    public GameObject timedButton;
-    public GameObject wavesButton;
-
-
     public void OnPlayButton()
-    {
-        playButton.SetActive(false);
-        timedButton.SetActive(true);
-        wavesButton.SetActive(true);
-    }
-
-    public void OnTimedButton()
-    {
-        SceneManager.LoadScene(1); // fix this to fit the correct scene if needed
-    }
-
-    public void OnWavesButton()
     {
         SceneManager.LoadScene(1); // fix this to fit the correct scene if needed
     }
